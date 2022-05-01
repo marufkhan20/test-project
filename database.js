dc914c567b09ed86d6e51c8b388b486f503eb0d3
unknown <mdm326040@gmail.com>
2022-05-01 08:41:14 +0600

    Big Changes

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const dbConnect = () => {
-	const DB_URL = 'mongodb+srv://326040Maruf:326040Maruf@cluster0.0wztu.mongodb.net/facebook';
+	const DB_URL = 'mongodb+srv://326040Maruf:326040Maruf@cluster0.0wztu.mongodb.net/facebook_test';
 	// database connection
 	mongoose.connect(DB_URL, {
 		useNewUrlParser: true,
